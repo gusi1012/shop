79a946e3c99dc3475994b628d736785c62c30fb9
Siam Gundermann <siam (at) gundermann.de>
2013-10-29 21:37:48 +0100

    Attribut nachname, vorname Getter, Setter, ToString, GetHashCode, Equals

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
@@ -2,5 +2,73 @@ package de.shop.kundenverwaltung.domain;
 
 public class Kunde {
 	private Long id;
+	private String nachname;
+	private String vorname;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public Kunde(Long id, String nachname, String vorname) {
+		super();
+		this.id = id;
+		this.nachname = nachname;
+		this.vorname = vorname;
+	}
+	@Override
+	public String toString() {
+		return "Kunde [id=" + id + ", nachname=" + nachname + ", vorname="
+				+ vorname + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nachname == null) ? 0 : nachname.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kunde other = (Kunde) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nachname == null) {
+			if (other.nachname != null)
+				return false;
+		} else if (!nachname.equals(other.nachname))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
+			return false;
+		return true;
+	}
+	
 
 }
