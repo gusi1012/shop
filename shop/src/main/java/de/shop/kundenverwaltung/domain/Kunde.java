65294b80297e80a0a0af3c685dd9546711278d00
Siam Gundermann <siam (at) gundermann.de>
2013-10-29 23:38:36 +0100

    Klasse Adresse,Kunde um Attribute erweitert

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
@@ -1,49 +1,171 @@
 package de.shop.kundenverwaltung.domain;
 
+import java.net.URI;
+import java.security.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.Property, property = "type")
+
+@XmlRootElement
 public class Kunde {
 	private Long id;
-	private String nachname;
+	
+	@NotNull(message ="{kunde.nachname.notNull}")
+	@Size(min = 2, max = 32, message= "{kunde.nachname.length}")
+	@Pattern(regexp = "[A-ZÄÖÜ][a-zäöüß]+")
 	private String vorname;
+	
+	@NotNull(message ="{kunde.nachname.notNull}")
+	@Size(min = 2, max = 32, message= "{kunde.nachname.length}")
+	@Pattern(regexp = "[A-ZÄÖÜ][a-zäöüß]+")
+	private String nachname;
+	private String email;
+	private String telefonnummer;
 	private Adresse adresse;
+	private String bankverbindung;
+	private Date registrierdatum;
+	private Timestamp erzeugt;
+	
+	@XmlTransient
+	private List<Bestellung> bestellungen;
+	
+	private URI bestellungUri;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNachname() {
-		return nachname;
-	}
-	public void setNachname(String nachname) {
-		this.nachname = nachname;
-	}
+
 	public String getVorname() {
 		return vorname;
 	}
+
 	public void setVorname(String vorname) {
 		this.vorname = vorname;
 	}
-	public Kunde(Long id, String nachname, String vorname) {
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefonnummer() {
+		return telefonnummer;
+	}
+
+	public void setTelefonnummer(String telefonnummer) {
+		this.telefonnummer = telefonnummer;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getBankverbindung() {
+		return bankverbindung;
+	}
+
+	public void setBankverbindung(String bankverbindung) {
+		this.bankverbindung = bankverbindung;
+	}
+
+	public Date getRegistrierdatum() {
+		return registrierdatum;
+	}
+
+	public void setRegistrierdatum(Date registrierdatum) {
+		this.registrierdatum = registrierdatum;
+	}
+
+	public Timestamp getErzeugt() {
+		return erzeugt;
+	}
+
+	public void setErzeugt(Timestamp erzeugt) {
+		this.erzeugt = erzeugt;
+	}
+
+	public List<Bestellung> getBestellungen() {
+		return bestellungen;
+	}
+
+	public void setBestellungen(List<Bestellung> bestellungen) {
+		this.bestellungen = bestellungen;
+	}
+
+	public URI getBestellungUri() {
+		return bestellungUri;
+	}
+
+	public void setBestellungUri(URI bestellungUri) {
+		this.bestellungUri = bestellungUri;
+	}
+
+	public Kunde(Long id, String vorname, String nachname, String email,
+			String telefonnummer, Adresse adresse, String bankverbindung,
+			Date registrierdatum, Timestamp erzeugt,
+			List<Bestellung> bestellungen, URI bestellungUri) {
 		super();
 		this.id = id;
-		this.nachname = nachname;
 		this.vorname = vorname;
+		this.nachname = nachname;
+		this.email = email;
+		this.telefonnummer = telefonnummer;
+		this.adresse = adresse;
+		this.bankverbindung = bankverbindung;
+		this.registrierdatum = registrierdatum;
+		this.erzeugt = erzeugt;
+		this.bestellungen = bestellungen;
+		this.bestellungUri = bestellungUri;
 	}
+
 	@Override
 	public String toString() {
-		return "Kunde [id=" + id + ", nachname=" + nachname + ", vorname="
-				+ vorname + "]";
+		return "Kunde [id=" + id + ", vorname=" + vorname + ", nachname="
+				+ nachname + ", email=" + email + ", telefonnummer="
+				+ telefonnummer + ", adresse=" + adresse + ", bankverbindung="
+				+ bankverbindung + ", registrierdatum=" + registrierdatum
+				+ ", erzeugt=" + erzeugt + ", bestellungen=" + bestellungen
+				+ ", bestellungUri=" + bestellungUri + "]";
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result
-				+ ((nachname == null) ? 0 : nachname.hashCode());
-		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -53,20 +175,10 @@ public class Kunde {
 		if (getClass() != obj.getClass())
 			return false;
 		Kunde other = (Kunde) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (email == null) {
+			if (other.email != null)
 				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nachname == null) {
-			if (other.nachname != null)
-				return false;
-		} else if (!nachname.equals(other.nachname))
-			return false;
-		if (vorname == null) {
-			if (other.vorname != null)
-				return false;
-		} else if (!vorname.equals(other.vorname))
+		} else if (!email.equals(other.email))
 			return false;
 		return true;
 	}
