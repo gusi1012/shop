8e5d51eb4f902e38132983d31d60cae8e7611f97
admin <admin@Tech-hska>
2013-10-30 16:00:28 +0100

    MailException , kundenserviceexception und Shopexception erstellt

---
+++ shop/src/main/java/de/shop/kundenverwaltung/service/KundenServiceException.java
@@ -0,0 +1,16 @@
+package de.shop.kundenverwaltung.service;
+
+import de.shop.util.ShopException;
+
+
+public abstract class KundenServiceException extends ShopException {
+	private static final long serialVersionUID = -2849585609393128387L;
+
+	public KundenServiceException(String msg) {
+		super(msg);
+	}
+	
+	public KundenServiceException(String msg, Throwable t) {
+		super(msg, t);
+	}
+}
