ba9f22a91a817d2c9ee6078416bf3d49ed16a06c
tran1013 <tran1013@hs-karlsruhe.de>
2013-10-31 12:35:57 +0100

    KundeResource comment gelöscht

--- shop/src/main/java/de/shop/kundenverwaltung/rest/KundeResource.java
+++ shop/src/main/java/de/shop/kundenverwaltung/rest/KundeResource.java
@@ -19,7 +19,6 @@ import javax.validation.Valid;
 //import javax.validation.constraints.Pattern;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-//import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
